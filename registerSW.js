d095feb9ca35c2f0ad5271d72c685bad15a1f514
bovlb <31326650+bovlb@users.noreply.github.com>
2025-05-11 11:34:48 -0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/kingdomino/sw.js', { scope: '/kingdomino/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
